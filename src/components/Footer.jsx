8280fd610a8d9fd3f6099e119e24deae3a14e643
TAREK <nasantarek6@gmail.com>
2025-05-30 10:04:53 +0200

    add page propos

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,8 +1,10 @@
 import '../styles/Footer.scss';
+import LOGO2 from '../assets/logo2.png';
 
 function Footer() {
   return (
     <footer className="footer">
+      <img src={LOGO2} alt="Kasa logo2" className="logo2" />
       <p>© 2020 Kasa. All rights reserved</p>
     </footer>
   );
--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -1,9 +1,33 @@
+import Banner from '../components/Banner';
+import Dropdown from '../components/Dropdown';
+import bannerAbout from '../assets/banner-about.jpg';
+import '../styles/About.scss';
+
 function About() {
   return (
-    <div>
-      <h1>À propos</h1>
+    <div className="about">
+      <Banner image={bannerAbout} alt="Montagnes paisibles" />
+      
+      <div className="about-dropdowns">
+        <Dropdown
+          title="Fiabilité"
+          content="Les annonces postées sur Kasa garantissent une fiabilité totale..."
+        />
+        <Dropdown
+          title="Respect"
+          content="La bienveillance fait partie des valeurs fondatrices de Kasa..."
+        />
+        <Dropdown
+          title="Service"
+          content="Nos équipes se tiennent à votre disposition pour vous fournir une expérience parfaite..."
+        />
+        <Dropdown
+          title="Sécurité"
+          content="La sécurité est la priorité de Kasa. Aussi bien pour nos hôtes que pour les voyageurs..."
+        />
+      </div>
     </div>
-  )
+  );
 }
 
-export default About
+export default About;
